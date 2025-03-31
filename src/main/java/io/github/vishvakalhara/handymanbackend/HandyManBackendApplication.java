458544a6e16602ce619669b00c71b5bffcef6073
vishva-kalhara <vishvakalhara@gmail.com>
2025-03-31 21:08:47 +0530

    init

---
+++ src/main/java/io/github/vishvakalhara/handymanbackend/HandyManBackendApplication.java
@@ -0,0 +1,13 @@
+package io.github.vishvakalhara.handymanbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandyManBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HandyManBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/vishvakalhara/handymanbackend/HandyManBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.vishvakalhara.handymanbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandyManBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
